2eafb365b1e51c3a2a9fc5e32a56371d12f8867d
TanLi2004 <92618681+TanLi2004@users.noreply.github.com>
2023-10-29 19:49:52 -0500

    version6

--- 1234.js
+++ 1234.js
@@ -1,64 +1,113 @@
-const audioPlayer = document.getElementById('audio-player');
-const nowPlaying = document.getElementById('now-playing');
-const albumCover = document.getElementById('album-cover');
-const playPauseBtn = document.getElementById('play-pause-btn');
-const progressBar = document.getElementById('progress-bar');
-
-function playMusic(songUrl, coverUrl, songTitle) {
-  if (audioPlayer.src !== songUrl) {
-    audioPlayer.src = songUrl;
-    albumCover.src = coverUrl || 'default-album-cover.jpg';
-    nowPlaying.textContent = songTitle || 'Unknown Song';
-    audioPlayer.load(); // 重新加载音频以确保能够自动播放
-
-    audioPlayer.play().then(() => {
-      playPauseBtn.innerHTML = '&#10074;&#10074;'; // 设置为播放状态的显示
-    }).catch(error => console.error("Playback failed", error));
-  }
-}
-
-audioPlayer.addEventListener('play', function() {
-    playPauseBtn.innerHTML = '&#10074;&#10074;';
-});
-
-audioPlayer.addEventListener('pause', function() {
-    playPauseBtn.innerHTML = '&#9654;';
-});
-
-playPauseBtn.addEventListener('click', function() {
-    if (audioPlayer.paused) {
-        audioPlayer.play();
-    } else {
-        audioPlayer.pause();
+document.addEventListener('DOMContentLoaded', function () {
+  const audioPlayer = document.getElementById('audio-player');
+  const nowPlaying = document.getElementById('now-playing');
+  const albumCover = document.getElementsByClassName('album-cover')[0];
+  const playPauseBtn = document.getElementById('play-pause-btn');
+  const prevBtn = document.getElementById('prev-btn');
+  const nextBtn = document.getElementById('next-btn');
+  const progressBar = document.getElementById('progress-bar');
+  
+  class Song {
+    constructor(src, cover, title) {
+      this.src = src;
+      this.cover = cover;
+      this.title = title;
     }
-});
-
-audioPlayer.addEventListener('timeupdate', updateProgressBar);
-
-function updateProgressBar() {
-  const percentage = (audioPlayer.currentTime / audioPlayer.duration) * 100;
-  progressBar.value = percentage;
-}
-
-progressBar.addEventListener('input', seek);
-
-function seek() {
-  const time = (progressBar.value * audioPlayer.duration) / 100;
-  audioPlayer.currentTime = time;
-}
-
-
-class Song {
-  constructor(src, cover, title) {
-    this.src = src;
-    this.cover = cover;
-    this.title = title;
   }
-}
+  
+  const songs = [
+    new Song('musica/daoxian.mp3', 'musica/daoxian.jpg', '稻香', '周杰伦'),
+    new Song('musica/2002.mp3', 'musica/2002.jpg', '2002', 'Anne Marie'),
+    new Song('musica/Something Just Like This.mp3', 'musica/somethink.jpg', 'Something Just Like This', 'The Chainsmokers'),
+    new Song('musica/Creep.mp3', 'musica/creep.jpg', 'Creep', 'Radiohead'),
+    new Song('musica/Nirvana.mp3', 'musica/narriva.jpg', 'Smells Like Teen Spirit', 'Nirvana'),
+    new Song('musica/Alright.mp3', 'musica/alright.jpg', 'Alright', 'Kendrick Lamar'),
+    new Song('musica/daoxian.mp3', 'musica/daoxian.jpg', 'Canción 18'),
+  ];
+  
+  let currentSongIndex = 0;
+  let playedSongs = [];  // 存储已播放歌曲的索引
+  
+  function getRandomSongIndex() {
+    if (songs.length > 1) {
+      let newSongIndex;
+      do {
+        newSongIndex = Math.floor(Math.random() * songs.length);
+      } while (newSongIndex === currentSongIndex || playedSongs.includes(newSongIndex));
+      return newSongIndex;
+    }
+    return 0;
+  }
+  
+  function playMusic(songUrl, coverUrl, songTitle) {
+    if (audioPlayer.src !== songUrl) {
+      audioPlayer.src = songUrl;
+      albumCover.src = coverUrl || 'default-album-cover.jpg';
+      nowPlaying.textContent = songTitle || 'Unknown Song';
+      audioPlayer.load();
+    }
+    if (audioPlayer.paused) {
+      audioPlayer.play().then(() => {
+        playPauseBtn.innerHTML = '&#10074;&#10074;';
+      }).catch(error => console.error("Playback failed", error));
+    } else {
+      audioPlayer.pause();
+    }
+  }
+  
+  function playNextSong() {
+    currentSongIndex = getRandomSongIndex();
+    const nextSong = songs[currentSongIndex];
+    playMusic(nextSong.src, nextSong.cover, nextSong.title);
+    playedSongs.push(currentSongIndex);
+    if (playedSongs.length > songs.length - 1) playedSongs.shift();  // 保持 playedSongs 的大小
+  }
+  
+  function playPreviousSong() {
+    if (playedSongs.length > 0) {
+      currentSongIndex = playedSongs.pop();  // 获取上一首歌曲的索引
+      const prevSong = songs[currentSongIndex];
+      playMusic(prevSong.src, prevSong.cover, prevSong.title);
+    }
+  }
+  
+  playPauseBtn.addEventListener('click', function() {
+    if (audioPlayer.src) {
+      if (audioPlayer.paused) {
+        audioPlayer.play();
+      } else {
+        audioPlayer.pause();
+      }
+    } else {
+      playNextSong();
+    }
+  });
 
-const songs = [
-  new Song('musica/daoxian.mp3', 'musica/daoxian.jpg', 'Canción 18')
-];
+  prevBtn.addEventListener('click', playPreviousSong);
+  nextBtn.addEventListener('click', playNextSong);
+  
+  audioPlayer.addEventListener('play', function() {
+    playPauseBtn.innerHTML = '&#10074;&#10074;';
+  });
+  
+  audioPlayer.addEventListener('pause', function() {
+    playPauseBtn.innerHTML = '&#9654;';
+  });
+  
+  audioPlayer.addEventListener('timeupdate', updateProgressBar);
+  
+  function updateProgressBar() {
+    const percentage = (audioPlayer.currentTime / audioPlayer.duration) * 100;
+    progressBar.value = percentage;
+  }
+  
+  progressBar.addEventListener('input', seek);
+  
+  function seek() {
+    const time = (progressBar.value * audioPlayer.duration) / 100;
+    audioPlayer.currentTime = time;
+  }
+});
 
 
 document.querySelectorAll('.song-card').forEach(card => {
@@ -90,23 +139,8 @@ if (audioPlayer.readyState >= 2) {
     audioPlayer.play();
   }, { once: true });
 }
-audioPlayer.play().catch(error => {
-  console.error("Playback failed", error);
-  alert("Playback failed. Please click the play button to start playing.");
-});
 
 
-if (audioPlayer.readyState >= 2) {
-  audioPlayer.play().catch(error => {
-    console.error("Playback failed", error);
-    alert("Playback failed. Please click the play button to start playing.");
-  });
-} else {
-  audioPlayer.addEventListener('canplay', function() {
-    audioPlayer.play().catch(error => {
-      console.error("Playback failed", error);
-      alert("Playback failed. Please click the play button to start playing.");
-    });
-  }, { once: true });
-}
+
+
 
